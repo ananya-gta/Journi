45d2492ef170554365a76da0d0a59089513f49c4
Ananya Gupta <anannya.gta@gmail.com>
2024-08-08 21:19:21 +0530

    Added Button component

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,10 @@ import App from "./App.jsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-const router = createBrowserRouter({
+const router = createBrowserRouter([{
   path: "/",
   element: <App />,
-});
+}]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
