445b26fd317bd4715418790f25aa3946b20789de
Ananya Gupta <anannya.gta@gmail.com>
2024-08-09 22:34:01 +0530

    completed Hero section

--- src/components/custom/Hero.jsx
+++ src/components/custom/Hero.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Hero() {
   return (
@@ -7,24 +8,23 @@ function Hero() {
         <span className="text-[#4242d8]">Plan Smarter, Travel Better: </span>
         Discover Your Next Trip with AI
       </h1>
-      <p className="text-xl text-gray-500 items-center">
+      <p className="text-xl text-gray-500 items-center sm:text-lg md:text-xl">
         Your ultimate AI trip planner and travel curator, creating custom
         itineraries perfectly aligned with your interests and budget.
       </p>
-      <a href="#_" class="relative inline-block text-lg group">
-        <span class="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
-          <span class="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
-          <span class="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
-          <span class="relative">Get Started</span>
-          
-        </span>
-        <span
-          class="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
-          data-rounded="rounded-lg"
-        ></span>
-
-        
-      </a>
+      <Link to={"/create-trip"}>
+        <a href="#_" class="relative inline-block text-lg group">
+          <span class="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+            <span class="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+            <span class="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
+            <span class="relative">Get started →</span>
+          </span>
+          <span
+            class="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
+            data-rounded="rounded-lg"
+          ></span>
+        </a>
+      </Link>
     </div>
   );
 }
