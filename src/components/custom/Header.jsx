c7fa313288d47b88f3f186326564e0e1fa660834
Ananya Gupta <anannya.gta@gmail.com>
2024-08-09 14:02:37 +0530

    Added Header

--- src/components/custom/Header.jsx
+++ src/components/custom/Header.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import React from "react";
+import { Button } from "../ui/button.jsx";
 
 function Header() {
   return (
-    <div className='flex space-x-2 mt-3 px-3'>
-        <img src="/logo.svg" alt="" className='w-10 h-10'/>
-        <h1 className=' font-bold text-3xl'>Journi</h1>
+    <div className="flex justify-between shadow-md p-3 ">
+      <div className="flex space-x-2">
+        <img src="/logo.svg" alt="" className="w-10 h-10" />
+        <h1 className=" font-bold text-3xl">Journi</h1>
+      </div>
+      <div>
+        <Button>Sign In</Button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
