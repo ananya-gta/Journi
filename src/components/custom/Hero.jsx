8a425207a10b2988648f6c6e89e15c5eb8c05258
Ananya Gupta <anannya.gta@gmail.com>
2024-08-09 21:13:25 +0530

    Completed Hero Section

--- src/components/custom/Hero.jsx
+++ src/components/custom/Hero.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
 
 function Hero() {
   return (
-    <div>Hero</div>
-  )
+    <div className="flex flex-col items-center mx-56 gap-9">
+      <h1 className="font-extrabold text-[60px] text-center mt-10">
+        <span className="text-[#4242d8]">Plan Smarter, Travel Better: </span>
+        Discover Your Next Trip with AI
+      </h1>
+      <p className="text-xl text-gray-500 items-center">
+        Your ultimate AI trip planner and travel curator, creating custom
+        itineraries perfectly aligned with your interests and budget.
+      </p>
+      <a href="#_" class="relative inline-block text-lg group">
+        <span class="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+          <span class="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+          <span class="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
+          <span class="relative">Get Started</span>
+          
+        </span>
+        <span
+          class="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
+          data-rounded="rounded-lg"
+        ></span>
+
+        
+      </a>
+    </div>
+  );
 }
 
-export default Hero
+export default Hero;
