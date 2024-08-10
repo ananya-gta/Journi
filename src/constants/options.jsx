91d77972de8ca1912d517e4ae6869c7145b7a826
Ananya Gupta <anannya.gta@gmail.com>
2024-08-10 19:39:24 +0530

    "Added state for place, updated GooglePlacesAutocomplete component, and added new form fields for trip duration, budget, and travel companions"

---
+++ src/constants/options.jsx
@@ -0,0 +1,51 @@
+export const SelectTravelsList = [
+  {
+    id: 1,
+    title: "Just Me",
+    description: "Solo journeys await",
+    icon: "✈",
+    people: "1",
+  },
+  {
+    id: 2,
+    title: "A Couple",
+    description: "Romantic getaways",
+    icon: "🥂",
+    people: "2",
+  },
+  {
+    id: 3,
+    title: "Family",
+    description: "Memorable family trips",
+    icon: "🏡",
+    people: "3 to 5 people",
+  },
+  {
+    id: 4,
+    title: "Friends",
+    description: "Adventures with friends",
+    icon: "⛵",
+    people: "6 to 8 people",
+  },
+];
+
+export const SelectBudgetOptions = [
+  {
+    id: 1,
+    title: "Economy",
+    description: "Save on travel",
+    icon: "💵",
+  },
+  {
+    id: 2,
+    title: "Standard",
+    description: "Moderate spendings",
+    icon: "💰",
+  },
+  {
+    id: 3,
+    title: "Premium",
+    description: "Luxury experience",
+    icon: "💸",
+  },
+];
--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -1,9 +1,14 @@
-import React from "react";
-import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
+import React, { useState } from "react";
+import GooglePlacesAutocomplete from "react-google-places-autocomplete";
+// import { Input } from "../components/ui/input.jsx";
+import { Input } from "../components/ui/input";
+import { SelectBudgetOptions } from "../constants/options";
 
 function CreateTrip() {
+  const [place, setPlace] = useState();
+
   return (
-    <div className="sm:px-10 md:px-32 lg:px-56 xl:px-10 px-5 mt-10">
+    <div className="sm:px-10 md:px-32 lg:px-56 px-5 mt-10">
       <h2 className="font-bold text-3xl">Create Your Ideal Travel Plan</h2>
       <p className="mt-3 text-gray-500 text-xl">
         Let us know your travel preferences and goals. Our smart AI generator
@@ -11,11 +16,52 @@ function CreateTrip() {
         dreams.
       </p>
 
-      <div className="mt-10">
+      <div className="mt-10 flex flex-col gap-10">
         <div>
-          <h2 className="text-xl my-3 font-medium">What is your destination?</h2>
-          <GooglePlacesAutocomplete apiKey={import.meta.env.VITE_GOOGLE_PLACE_API_KEY}/>
+          <h2 className="text-xl my-3 font-medium">
+            What is your destination?
+          </h2>
+          <GooglePlacesAutocomplete
+            apiKey={import.meta.env.VITE_GOOGLE_PLACE_API_KEY}
+            selectProps={{
+              place,
+              onChange: (value) => {
+                setPlace(value);
+                console.log(value);
+              },
+            }}
+          />
         </div>
+
+        <div>
+          <h2 className="text-xl my-3 font-medium">
+            For how many days are you planning your trip?
+          </h2>
+          <Input placeholder={"Ex.3"} type="number" />
+        </div>
+
+        <div>
+          <h2 className="text-xl my-3 font-medium">
+          What is your budget?
+          </h2>
+          <div>
+            {SelectBudgetOptions.map((item, index)=> {
+              <div key={index}>
+                <h2>{item.icon}</h2>
+
+              </div>
+            })}
+          </div>
+        </div>
+
+        <div>
+          <h2 className="text-xl my-3 font-medium">
+          Who do you plan on traveling with on your next adventure?
+          </h2>
+          <Input placeholder={"Ex.3"} type="number" />
+        </div>
+
+
       </div>
     </div>
   );
