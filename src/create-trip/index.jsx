1fa75f2ce08cbe805e189ecf6db1548a5716fb15
Ananya Gupta <anannya.gta@gmail.com>
2024-08-12 23:40:55 +0530

    completed create trip page

--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
 import GooglePlacesAutocomplete from "react-google-places-autocomplete";
 import { Input } from "../components/ui/input";
-import { SelectBudgetOptions, SelectTravelsList } from "../constants/options.jsx";
+import {
+  SelectBudgetOptions,
+  SelectTravelsList,
+} from "../constants/options.jsx";
 
 function CreateTrip() {
   const [place, setPlace] = useState();
@@ -40,14 +43,16 @@ function CreateTrip() {
         </div>
 
         <div>
-          <h2 className="text-xl my-3 font-medium">
-          What is your budget?
-          </h2>
-          <div>
-            {SelectBudgetOptions.map((item, index)=> (
-              <div key={index}>
-                <h2>{item.icon}</h2>
-
+          <h2 className="text-xl my-3 font-medium">What is your budget?</h2>
+          <div className="grid grid-cols-3 gap-5 mt-5">
+            {SelectBudgetOptions.map((item, index) => (
+              <div
+                key={index}
+                className="p-4 border cursor-pointer rounded-lg hover:shadow-lg"
+              >
+                <h2 className="text-4xl">{item.icon}</h2>
+                <h2 className="font-bold text-lg">{item.title}</h2>
+                <h2 className="text-sm text-gray-500">{item.description}</h2>
               </div>
             ))}
           </div>
@@ -55,18 +60,30 @@ function CreateTrip() {
 
         <div>
           <h2 className="text-xl my-3 font-medium">
-          Who do you plan on traveling with on your next adventure?
+            Who do you plan on traveling with on your next adventure?
           </h2>
-          <div>
-            {SelectTravelsList.map((item, i)=> (
-              <div key={i}>
-                <h2>{item.icon}</h2>
+          <div className="grid grid-cols-4 gap-5 mt-5">
+            {SelectTravelsList.map((item, i) => (
+              <div
+                key={i}
+                className="p-4 border cursor-pointer rounded-lg hover:shadow-lg"
+              >
+                <h2 className="text-4xl">{item.icon}</h2>
+                <h2 className="font-bold text-lg">{item.title}</h2>
+                <h2 className="text-sm text-gray-500">{item.description}</h2>
               </div>
             ))}
           </div>
         </div>
-
-
+      </div>
+      <div className="my-10 justify-end flex">
+        <a
+          href="#_"
+          class="relative rounded px-5 py-2.5 overflow-hidden group bg-green-500 relative hover:bg-gradient-to-r hover:from-green-500 hover:to-green-400 text-white hover:ring-2 hover:ring-offset-2 hover:ring-green-400 transition-all ease-out duration-300"
+        >
+          <span class="absolute right-0 w-8 h-32 -mt-12 transition-all duration-1000 transform translate-x-12 bg-white opacity-10 rotate-12 group-hover:-translate-x-40 ease"></span>
+          <span class="relative">Generate Trip</span>
+        </a>
       </div>
     </div>
   );
