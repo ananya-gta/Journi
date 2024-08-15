8e238457d040f0c8d79032f154cfbc04f9b8118d
Ananya Gupta <anannya.gta@gmail.com>
2024-08-15 16:03:43 +0530

    Added emojis to "Create Your Ideal Travel Plan" header text.

--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -11,7 +11,7 @@ function CreateTrip() {
 
   return (
     <div className="sm:px-10 md:px-32 lg:px-56 px-5 mt-10">
-      <h2 className="font-bold text-3xl">Create Your Ideal Travel Plan</h2>
+      <h2 className="font-bold text-3xl">Create Your Ideal Travel Plan 🏕️🌴</h2>
       <p className="mt-3 text-gray-500 text-xl">
         Let us know your travel preferences and goals. Our smart AI generator
         will produce a custom plan that fits your unique tastes and travel
